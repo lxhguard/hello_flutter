5fbab2e9cfded4a001689af58f8c07ee2ff7422b
liuxinhai <liuxinhai@xiaomi.com>
2020-04-21 13:46:50 +0800

    feat: customize Widget with Class

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,24 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(
-    Center(
+  runApp(App());
+}
+/**
+ * 有需要修改数据的小部件去继承StatefulWidget
+ * 快捷键sfw
+ * 不需要修改数据的小部件去继承StatelessWidget
+ * 快捷键ssw
+ */
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
       child: Text(
         'hello',
         textDirection: TextDirection.ltr,
       )
-    )
-  );
+    );
+  }
 }
